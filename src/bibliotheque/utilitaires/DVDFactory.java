af5c0c3c22b548d65482fc902f4948de662009d6
Ahmed <Ahmed@gmail.com>
2024-04-23 14:26:28 +0200

    commit 5

--- src/bibliotheque/utilitaires/DVDFactory.java
+++ src/bibliotheque/utilitaires/DVDFactory.java
@@ -1,38 +1,76 @@
 package bibliotheque.utilitaires;
 
 import bibliotheque.metier.DVD;
+import bibliotheque.metier.Exemplaire;
 import bibliotheque.metier.Ouvrage;
+import bibliotheque.mvc.GestionMVC;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.*;
 
-public class DVDFactory extends OuvrageFactory{
+import static bibliotheque.utilitaires.Utilitaire.affListe;
+import static bibliotheque.utilitaires.Utilitaire.choixListe;
+
+public abstract class DVDFactory extends OuvrageFactory{
     protected long code;
     protected LocalTime dureeTotale;
     protected byte nbreBonus;
 
-    public Ouvrage create(){
-        super.base();
-        System.out.println("code : ");
-        code= sc.nextLong();
-        dureeTotale=Utilitaire.lecTime();
-        nbreBonus= sc.nextByte();
-        DVD dvd =new DVD(titre,ageMin,dateParution,prixLocation,langue,genre,code,dureeTotale,nbreBonus);
-        System.out.println("autres langues");
-        List<String> langues = Arrays.asList("anglais","français","italien","allemand","fin");
-        int choix;
-        do{
-            choix=Utilitaire.choixListe(langues);
-            if(choix==langues.size())break;
-            dvd.getAutresLangues().add(langues.get(choix-1));//TODO vérifier unicité ou utiliser set et pas de doublon avec langue d'origine
-        }while(true);
-        System.out.println("sous-titres");
-        do{
-            choix=Utilitaire.choixListe(langues);
-            if(choix==langues.size())break;
-            dvd.getSousTitres().add(langues.get(choix-1));//TODO vérifier unicité ou utiliser set
-        }while(true);
-        return dvd;
+    public void ajouter() {
+        Exemplaire a;
+        do {
+            try {
+                System.out.println("Matricule :");
+                String mat = sc.nextLine();
+                if (mat.isEmpty()) {
+                    System.out.println("Erreur : Le matricule ne peut être vide.");
+                    continue;  // Recommence la boucle si le matricule est vide
+                }
+
+                System.out.println("Description :");
+                String descr = sc.nextLine();
+                if (descr.isEmpty()) {
+                    System.out.println("Erreur : La description ne peut être vide.");
+                    continue;  // Recommence la boucle si la description est vide
+                }
+
+                System.out.println("Ouvrage :");
+                List<Ouvrage> lo = GestionMVC.ov.getAll();  // Assume getAll() récupère tous les ouvrages
+                // Trie les ouvrages par titre
+                lo.sort(Comparator.comparing(Ouvrage::getTitre));
+                affListe(lo);
+
+                int choixOuvrage = choixListe(lo);
+                if (choixOuvrage <= 0 || choixOuvrage > lo.size()) {
+                    System.out.println("Choix invalide.");
+                    continue;  // Recommence la boucle si le choix est invalide
+                }
+                Ouvrage ouvrageChoisi = lo.get(choixOuvrage - 1);
+
+                System.out.println("Rayon :");
+                List<Rayon> lr = GestionMVC.rv.getAll();  // Assume getAll() récupère tous les rayons
+                // Trie les rayons par code
+                lr.sort(Comparator.comparing(Rayon::getCodeRayon));
+                affListe(lr);
+
+                int choixRayon = choixListe(lr);
+                if (choixRayon <= 0 || choixRayon > lr.size()) {
+                    System.out.println("Choix invalide.");
+                    continue;  // Recommence la boucle si le choix est invalide
+                }
+                Rayon rayonChoisi = lr.get(choixRayon - 1);
+
+                a = new Exemplaire(mat, descr, ouvrageChoisi);
+                a.setRayon(rayonChoisi);
+                controller.add(a);
+                System.out.println("Exemplaire ajouté avec succès.");
+                break;  // Sort de la boucle après un ajout réussi
+            } catch (Exception e) {
+                System.out.println("Une erreur est survenue : " + e.getMessage());
+                continue;  // En cas d'erreur, permet de recommencer la saisie
+            }
+        } while (true);
     }
+
 }
